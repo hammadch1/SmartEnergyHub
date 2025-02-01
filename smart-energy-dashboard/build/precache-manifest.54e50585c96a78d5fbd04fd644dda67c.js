dd76dd74dec31da644d0b6b948f11f860a7d1157
Muhammad Hammad Chaudhary <hammadch1@outlook.com>
2025-02-01 21:02:29 +0100

    build again

---
+++ smart-energy-dashboard/build/precache-manifest.54e50585c96a78d5fbd04fd644dda67c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccedca574e5dc7ddeb078f65a5a6ff10",
+    "url": "/SmartEnergyHub/index.html"
+  },
+  {
+    "revision": "30052a339112ce6ed833",
+    "url": "/SmartEnergyHub/static/js/2.fcb5700c.chunk.js"
+  },
+  {
+    "revision": "88552c73a08d6badf45f",
+    "url": "/SmartEnergyHub/static/js/main.d72ff61b.chunk.js"
+  },
+  {
+    "revision": "ebe25cddf008ada37c9c",
+    "url": "/SmartEnergyHub/static/js/runtime~main.9e691221.js"
+  }
+]);
