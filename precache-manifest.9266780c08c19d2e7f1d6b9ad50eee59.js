9c7e4f8ef2805bc4ab7f9028f101c7accdf4e61f
Muhammad Hammad Chaudhary <hammadch1@outlook.com>
2025-02-01 21:25:25 +0100

    Updates

---
+++ precache-manifest.9266780c08c19d2e7f1d6b9ad50eee59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e433c1569fdc17051bda88cb68320a4",
+    "url": "/SmartEnergyHub/index.html"
+  },
+  {
+    "revision": "98aaf5427e8880fe8283",
+    "url": "/SmartEnergyHub/static/js/2.82e63adf.chunk.js"
+  },
+  {
+    "revision": "c610699dc6001decc6ee",
+    "url": "/SmartEnergyHub/static/js/main.5c24fff7.chunk.js"
+  },
+  {
+    "revision": "ebe25cddf008ada37c9c",
+    "url": "/SmartEnergyHub/static/js/runtime~main.9e691221.js"
+  }
+]);
